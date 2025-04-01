669b2a844819a7d7f6a72cd7aac9dd4a4f21dfa7
Rasel Miah <rasel6041@gmail.com>
2025-04-02 00:58:14 +0100

    first commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,16 +4,11 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <section
-      className="relative bg-cover bg-center h-[80vh] lg:h-[70vh] flex items-center justify-center text-white overflow-hidden"
+      className="relative bg-cover bg-center h-[85vh] lg:h-[70vh] md:h-[80vh] sm:h-[90vh] flex items-center justify-center text-white overflow-hidden"
       style={{ backgroundImage: "url('/her3.jpg')" }}
     >
-     
-      {/* Dark overlay for better text contrast */}
-      <div className="relative z-10 text-center px-6 sm:px-12 w-full">
-        {/* Main Text Box */}
-
-        <div className="lg:mr-[200px] max-w-md mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[550px] sm:h-[600px]">
-          <h1 className="text-4xl sm:text-3xl font-bold leading-tight mb-6 text-left">
+        <div className="lg:mr-[200px] max-w-md mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[470px]">
+          <h1 className="text-2xl sm:text-3xl font-bold leading-tight mb-6 text-left">
          {/*  <House className="text-[#fd6d15] font-extrabold" style={{ fontSize: '300px' }} /> */}
 
 
@@ -23,11 +18,11 @@ const Hero = () => {
             <br />
             Meets Comfort
           </h1>
-          <p className="text-lg sm:text-xl mb-6 text-left">
+          <p className="text-lg sm:text-md mb-6 text-left">
             Specialist in customized<br /> accommodation solutions <br />for companies
             throughout Sweden
           </p>
-          <p className="text-md sm:text-lg mb-8 text-left">
+          <p className="text-lg sm:text-md mb-8 text-left">
             Is your company looking for accommodation<br /> for a longer construction
             project <br />or<br /> a shorter assignment?
           </p>
@@ -41,8 +36,6 @@ const Hero = () => {
           </div>
         </div>
 
-        {/* Call to Action Button */}
-      </div>
     </section>
   );
 };
