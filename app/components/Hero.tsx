946b8a04818a5831e8631432e2c4f82f38c36972
Rasel Miah <rasel6041@gmail.com>
2025-04-02 01:15:05 +0100

    first commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       className="relative bg-cover bg-center h-[85vh] lg:h-[70vh] md:h-[80vh] sm:h-[90vh] flex items-center justify-center text-white overflow-hidden"
       style={{ backgroundImage: "url('/her3.jpg')" }}
     >
-        <div className="lg:mr-[200px] max-w-sm mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[480px]">
+        <div className="lg:mr-[200px] max-w-[372px] mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[480px]">
           <h1 className="text-2xl sm:text-3xl font-bold leading-tight mb-6 text-left">
          {/*  <House className="text-[#fd6d15] font-extrabold" style={{ fontSize: '300px' }} /> */}
 
