22b65bf977f8fc423a4aa23a6b3e3f141fbf487d
Rasel Miah <rasel6041@gmail.com>
2025-04-04 10:40:31 +0100

    contact page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -7,29 +7,29 @@ const Services = () => {
         className="relative bg-cover bg-center h-[85vh] sm:h-[70vh] flex items-center justify-center text-[#7B5B4C] overflow-hidden"
         style={{ backgroundImage: "url('/ser.jpg')" }}
       >
-        <div className="lg:mr-[300px] w-[322px] sm:w-[400px] mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[450px] sm:h-[484px]">
-          {/* <h1 className="text-2xl sm:text-3xl font-bold leading-tight mb-6 text-left">
-            Our
+        <div className="lg:mr-[300px] w-[280px] sm:w-[400px] mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[350px] sm:h-[484px]">
+          <h1 className="text-2xl sm:text-3xl font-bold leading-tight mb-6 text-left">
+            Looking for
             <br />
-            Services
+            Flexible,
             <br />
-            At stayspace
+            Affordable,
+            <br />
+            and Quality
+            <br />
+            Accommodation for Your Team
+            <br />
+            with StaySpace.
           </h1>
-          <p className="text-md md:text-lg mb-6 text-left">
-            To
-            <br /> Know
-            <br />
-            More about
-          </p>
 
           <div className="text-center mt-6">
             <Link
-              href="/contact"
+              href="/contactus"
               className="px-6 py-3 bg-[#fd6d15] hover:bg-red-400 text-[#7B5B4C] font-semibold transition-all"
             >
               Contact Us
             </Link>
-          </div> */}
+          </div>
         </div>
       </section>
 
@@ -37,19 +37,18 @@ const Services = () => {
       <section className="py-16 bg-gray-50">
         <div className="container mx-auto px-6 sm:px-12 lg:px-32">
           <h2 className="text-3xl sm:text-4xl font-bold text-center text-[#7B5B4C] mb-10">
-            Our Services At  StaySpace
+            Our Services At StaySpace
           </h2>
           {/* Grid container for 3 cards */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* First Card */}
             <div className="w-full opacity-80 p-8 text-[#7B5B4C] sm:h-[450px] md:h-[484px] shadow-sm">
               <h1 className="text-lg md:text-3xl font-bold leading-tight text-left">
-              StaySpace 
+                StaySpace
               </h1>
               <p className="text-sm md:text-lg mb-6 text-justify">
-                specializes in providing corporate accommodation,
-                making it easy for you to find the right accommodation for your
-                employees.
+                specializes in providing corporate accommodation, making it easy
+                for you to find the right accommodation for your employees.
               </p>
               <p className="text-sm md:text-lg mb-6 text-justify">
                 we offer tailor-made solutions for both short and long-term
@@ -62,8 +61,7 @@ const Services = () => {
             {/* Second Card */}
             <div className="w-full opacity-80 p-8 text-[#7B5B4C] sm:h-[450px] md:h-[484px] shadow-sm">
               <h1 className="text-lg sm:text-3xl font-bold leading-tight text-left">
-              
-               At StaySpace
+                At StaySpace
               </h1>
               <p className="text-sm md:text-lg mb-6 text-justify">
                 We tailor our solutions to suit the different needs and budgets
