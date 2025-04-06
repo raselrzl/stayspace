445221792248143a469289064f9036718867651a
Rasel Miah <rasel6041@gmail.com>
2025-04-06 14:49:37 +0100

    form submitted successfully

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -8,7 +8,7 @@ const Services = () => {
         style={{ backgroundImage: "url('/ser.jpg')" }}
       >
         <div className="rounded-lg lg:mr-[300px] w-[280px] sm:w-[400px] mx-auto bg-white opacity-72 p-8 text-[#7B5B4C] h-[350px] sm:h-[484px]">
-          <h1 className="text-2xl sm:text-3xl font-bold leading-tight mb-6 text-left">
+          <h1 className="text-xl sm:text-2xl font-bold leading-tight mb-6 text-left">
             Looking for
             <br />
             Flexible,
@@ -36,21 +36,21 @@ const Services = () => {
       {/* New Section Below */}
       <section className="py-16 bg-gray-50">
         <div className="container mx-auto px-6 sm:px-12 lg:px-32">
-          <h2 className="text-3xl sm:text-4xl font-bold text-center text-[#7B5B4C] mb-10">
+          <h2 className="text-xl sm:text-2xl font-bold text-center text-[#7B5B4C] mb-10">
             Our Services At StaySpace
           </h2>
           {/* Grid container for 3 cards */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* First Card */}
             <div className="w-full opacity-80 p-8 text-[#7B5B4C] sm:h-[450px] md:h-[484px] shadow-sm">
-              <h1 className="text-lg md:text-3xl font-bold leading-tight text-left">
+              <h1 className="text-xl md:text-2xl font-bold leading-tight text-left">
                 StaySpace
               </h1>
-              <p className="text-sm md:text-lg mb-6 text-justify">
+              <p className="text-md md:text-lg mb-6 text-justify">
                 specializes in providing corporate accommodation, making it easy
                 for you to find the right accommodation for your employees.
               </p>
-              <p className="text-sm md:text-lg mb-6 text-justify">
+              <p className="text-md md:text-lg mb-6 text-justify">
                 we offer tailor-made solutions for both short and long-term
                 needs, with a wide range of attractive and fully equipped
                 apartments. Let us handle the accommodation process so you can
@@ -60,16 +60,16 @@ const Services = () => {
 
             {/* Second Card */}
             <div className="w-full opacity-80 p-8 text-[#7B5B4C] sm:h-[450px] md:h-[484px] shadow-sm">
-              <h1 className="text-lg sm:text-3xl font-bold leading-tight text-left">
+              <h1 className="text-xl sm:text-2xl font-bold leading-tight text-left">
                 At StaySpace
               </h1>
-              <p className="text-sm md:text-lg mb-6 text-justify">
+              <p className="text-md md:text-lg mb-6 text-justify">
                 We tailor our solutions to suit the different needs and budgets
                 of companies. We offer a wide range of accommodation options,
                 from high-comfort apartments and houses to more economical
                 options such as hostels.
               </p>
-              <p className="text-sm md:text-lg mb-6 text-justify">
+              <p className="text-md md:text-lg mb-6 text-justify">
                 Whether you are looking for accommodation for a longer project
                 or a shorter stay, we strive to deliver tailor-made solutions
                 that combine quality and cost-effectiveness.
@@ -78,10 +78,10 @@ const Services = () => {
 
             {/* Third Card */}
             <div className="w-full opacity-80 p-8 text-[#7B5B4C] sm:h-[450px] md:h-[484px] shadow-sm">
-              <h1 className="text-lg sm:text-3xl font-bold leading-tight mb-2 text-left">
+              <h1 className="text-xl sm:text-2xl font-bold leading-tight mb-2 text-left">
                 To make it easier for our guests, we offer 24/7 support
               </h1>
-              <p className="text-sm md:text-lg mb-6 text-justify">
+              <p className="text-md md:text-lg mb-6 text-justify">
                 We are always available to assist and answer questions, allowing
                 our guests to focus on what really matters during their stay.
               </p>
@@ -92,10 +92,10 @@ const Services = () => {
 
       <section className="bg-[#fdfaf5] py-16 px-4 sm:px-12">
         <div className="max-w-screen-xl mx-auto text-center">
-          <h2 className="text-3xl font-bold text-[#7B5B4C] mb-8">
+          <h2 className="text-xl font-bold text-[#7B5B4C] mb-8">
             Always Included in Your Stay
           </h2>
-          <p className="text-lg text-[#7B5B4C] mb-8">
+          <p className="text-md text-[#7B5B4C] mb-8">
             Enjoy the full range of modern amenities and services that make your stay comfortable, with everything you need, included.
           </p>
 
@@ -145,10 +145,10 @@ const Services = () => {
 
       <section className="bg-[#fdfaf5] py-16 px-4 sm:px-12">
         <div className="max-w-screen-xl mx-auto text-center">
-          <h2 className="text-3xl font-bold text-[#7B5B4C] mb-8">
+          <h2 className="text-xl font-bold text-[#7B5B4C] mb-8">
             Up Your Stay with Our Extra Services
           </h2>
-          <p className="text-lg text-[#7B5B4C] mb-8">
+          <p className="text-md text-[#7B5B4C] mb-8">
             Take your stay to the next level with our additional services, designed for your comfort and convenience.
           </p>
 
