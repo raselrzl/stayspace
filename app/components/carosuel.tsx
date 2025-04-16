61b1de263210917074160ff45f6c9f9c1e92a141
Rasel Miah <rasel6041@gmail.com>
2025-04-16 23:19:41 +0100

    imageAlbum

--- app/components/carosuel.tsx
+++ app/components/carosuel.tsx
@@ -1,25 +1,27 @@
-import * as React from "react"
+import * as React from "react";
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
 export function CarouselCompany() {
   const images = [
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-    "/stayspace.png",
-  ]
+    "/partner/peab.jpg",
+    "/partner/eks.webp",
+    "/partner/peab.jpg",
+    "/partner/hyres.jpg",
+
+    "/partner/peab.jpg",
+
+    "/partner/hyres.jpg",
+
+    "/partner/peab.jpg",
+  ];
 
   return (
     <Carousel
@@ -32,13 +34,15 @@ export function CarouselCompany() {
             key={index}
             className="basis-1/2 md:basis-1/3 flex justify-center" // Adjusted basis here
           >
-            <div className="px-0"> {/* Removed the padding */}
-              <Card className="rounded-none border-1 border-gray-100">
+            <div className="px-0">
+              {" "}
+              {/* Removed the padding */}
+              <Card className="rounded-none border-none shadow-none">
                 <CardContent className="flex items-center justify-center p-2">
                   <img
                     src={src}
                     alt={`Logo ${index + 1}`}
-                    className="h-auto w-full object-contain"
+                    className="h-24 w-[180px] object-contain"
                   />
                 </CardContent>
               </Card>
@@ -48,13 +52,9 @@ export function CarouselCompany() {
       </CarouselContent>
 
       {/* Previous Button */}
-      <CarouselPrevious
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2 text-white bg-black rounded-full opacity-80 hover:opacity-100 transition-all"
-      />
+      <CarouselPrevious className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2 text-white bg-black rounded-full opacity-80 hover:opacity-100 transition-all" />
       {/* Next Button */}
-      <CarouselNext
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-white bg-black rounded-full opacity-80 hover:opacity-100 transition-all"
-      />
+      <CarouselNext className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-white bg-black rounded-full opacity-80 hover:opacity-100 transition-all" />
     </Carousel>
-  )
+  );
 }
